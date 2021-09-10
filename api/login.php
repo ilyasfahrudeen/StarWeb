8e0e888c09f9e31c48907cae26bd425840ca262f
ilyas fahrudeen <ilyas.fahrudeen@gmail.com>
2021-09-10 20:45:31 +0530

    login

--- api/login.php
+++ api/login.php
@@ -2,33 +2,37 @@
 header("Content-type: application/json; charset=utf-8");
 include '../conndb.php';
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
-$json = file_get_contents('php://input');
-$obj = json_decode($json);
-$phone_number = $obj->{'phone_number'};
-$token = $obj->{'token'};
+    $json = file_get_contents('php://input');
+    $obj = json_decode($json,true);
+    $phone_number = $obj['phone_number'];
+ $token = $obj['token'];
+ $name = $obj['name'];
 
-$check_query = "SELECT * FROM `user_details` WHERE EXISTS (SELECT * FROM `user_details` WHERE phone_number=".$phone_number.")";
+$check_query = "SELECT `phone_number` FROM `user_details` WHERE `phone_number`='".$phone_number."'";
 $result = mysqli_query($con, $check_query);
-
-$check_exist = mysqli_num_rows($result);
-
-if($check_exist>0){
+if(mysqli_num_rows($result)>0){
+    $update_token = "UPDATE `user_details` SET `token`='".$token."' WHERE phone_number=".$phone_number;
+    mysqli_query($con,$update_token);
     $user_details_query = "SELECT * FROM `user_details` WHERE phone_number=".$phone_number;
-    $user_details = mysqli_query($con, $user_details_query);
-foreach($user_details as $row){
-    $fullar = "{ status: 1, message:'User found!', user_details: ".json_encode($row)." }";
-    echo $fullar;}
+        $user_details = mysqli_query($con, $user_details_query);
+    foreach($user_details as $row){
+        $fullar = "{ status: 1, message:'User found!', user_details: ".json_encode($row)." }";
+        echo $fullar;}
 }else{
-    $newUserQuery = "INSERT INTO `user_details`(`phone_number`, `token`) VALUES (".$phone_number.",".$token.")";
+    $newUserQuery = "INSERT INTO `user_details` (`phone_number`, `token`, `name`) VALUES ('".$phone_number."','".$token."','".$name."')";
     if ($con->query($newUserQuery) === TRUE) {
-        $newUser = array('phone_number'=>$phone_number, 'token'=>$token);
+        $last_id = $con->insert_id;
+        $newUser = array('user_id'=>$last_id, 'phone_number'=>$phone_number, 'token'=>$token, 'name'=>$name);
         $fullar = "{ status: 1, message:'New user added!', user_details: ".json_encode($newUser)." }";
         echo $fullar;
     } else {
         $fullar = "{ status: 0, message:'". $con->error."', user_details: [] }";
         echo $fullar;
-    } }
+    }
+}
+
   }
 ?>
